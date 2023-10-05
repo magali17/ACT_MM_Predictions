61a0cbb31b79177713dba25f3851db2d551a6ac7
magali17 <magalipdx@gmail.com>
2023-10-04 20:45:07 -0700

    ds

--- cohort_predictions.bash
+++ cohort_predictions.bash
@@ -1,20 +1,19 @@
-# script generates cohort predictions & saves them in the correct format for KPRI
-## bash B_cohort_predictions.bash
+script generates cohort predictions & saves them in the correct format for KPRI
+#bash B_cohort_predictions.bash
 
 #############################################################################################
-# NanoScan bins
-Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_10_100.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_10_100 rda
-
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_11.5.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_11.5 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_15.4.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_15.4 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_20.5.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_20.5 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_27.4.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_27.4 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_36.5.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_36.5 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_48.7.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_48.7 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_64.9.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_64.9 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_86.6.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_86.6 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_115.5.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_115.5 rda
-# Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_154.0.rda data/cohort/dr0357_cohort_covar_20220404.csv output/cohort/ns_154.0 rda
+NanoScan bins
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_10_100.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_10_100 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_11.5.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_11.5 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_15.4.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_15.4 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_20.5.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_20.5 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_27.4.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_27.4 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_36.5.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_36.5 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_48.7.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_48.7 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_64.9.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_64.9 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_86.6.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_86.6 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_115.5.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_115.5 rda
+Rscript 1_make_predictions.R data/output/specific_pollutants/annual_avg_ns_154.0.rda data/dr0357_cohort_covar_20220404_in_mm_area.rda output/cohort/ns_154.0 rda
 
 #############################################################################################
 
